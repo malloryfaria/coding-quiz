e01f783ba292b64c206059eed2777c7ebc700e1e
Mallory <mallory.faria@gmail.com>
2021-01-02 21:57:23 -0500

    saving progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,35 +4,10 @@ var questionIndex = 0;
 var timerEl = document.getElementById("countdown");
 var mainEl = document.getElementById("mainGame");
 var startBtn = document.getElementById("start");
-var timeRemaining = document.getElementById("timeRemaining");
 var message = "You have run out of time!";
 var penalty = "10";
-var createUl = document.createElement("ul");
 
-
-
-// GIVEN I am taking a code quiz WHEN I click the start button THEN a timer starts and I am presented with a question
-    // timer function
-    // TODO: Timer that counts down from x
-        function countdown() {
-            var timeLeft = 20;
-        
-            // Use the setInterval() method to call a function to be executed every 1000 milliseconds (every 1 second)
-            var timeInterval = setInterval(function() {
-            if(timeLeft >= 1) {
-                timerEl.textContent = timeLeft;
-                timeLeft -= 1;
-            }
-            else if(timeLeft === 0){
-                timerEl.textContent = "";
-                clearInterval(timeInterval);
-                console.log("I'm here");
-                displayMessage();
-            }
-        
-            function displayMessage() {
-                alert(message);
-            }
+startQuiz();
 
     // questions index
         var questions = [
@@ -64,19 +39,81 @@ var createUl = document.createElement("ul");
         ]; 
 
     // Display question to the user
-    function displayQuestion(questionIndex) {
+    function startQuiz(questionIndex) {
         // Clear existing question from the page
         mainEl.innerHTML = "";
-        createUl.innerHTML = "";
 
         // Loop to go through all the questions
         for(var i = 0; i < questions.length; i++) {
             // display the selected question title
-            var question = questions[questionIndex].title;
+            var title = document.createElement("div");
+            title.setAttribute("id", "title");
+            title.textContent = questions[questionIndex].title;
             // display the selected question's choices
-            var choices = questions[questionIndex].choices;
+            var choices = document.createElement("li");
+            choices.setAttribute("id", "choices");
+            choices.textContent = questions[questionIndex].choices;
+            // add the content to the page
+            mainEl.textContent = title;
+            mainEl.textContent = choices;
         }
-    }
+            mainEl.appendChild(document.createElement("li"));
+            choices.addEventListener("click", (compareAnswer));
+            return;
+        };
+
+        function compareAnswer(event) {
+            var element = event.target;
+            if (element.matches("li")) {
+                var createDiv = document.createElement("div");
+                createDiv.setAttribute("id", "createDiv");
+                // If answer is correct
+                if (element.textContent === questions[questionIndex].answer) {
+                    score ++;
+                    createDiv.textContent = "That is correct!"
+                }
+                else {
+                    // If answer is incorrect, remove 10 seconds from the timer
+                    timerEl = timerEl - penalty;
+                    createDiv.textContent = "That is incorrect! The correct answer is: " + questions[questionIndex].answer;
+                }
+            }
+            // move on to the next question
+            questionIndex ++;
+
+
+            function quizComplete () {
+                if (questionIndex >= questions.length) {
+                    createDiv.textContent = "Quiz complete! Your score was: " + score;    
+                }
+                else {
+                    mainEl.appendChild(createDiv);
+                }
+            }
+        };
+
+    // GIVEN I am taking a code quiz WHEN I click the start button THEN a timer starts and I am presented with a question
+        // timer function
+        // TODO: Timer that counts down from 90
+        function countdown() {
+            var timeLeft = 20;
+        
+            // Use the setInterval() method to call a function to be executed every 1000 milliseconds (every 1 second)
+            var timeInterval = setInterval(function() {
+            if(timeLeft >= 1) {
+                timerEl.textContent = timeLeft;
+                timeLeft -= 1;
+            }
+            else if(timeLeft === 0){
+                timerEl.textContent = "";
+                clearInterval(timeInterval);
+                console.log("I'm here");
+                displayMessage();
+            }
+        
+            function displayMessage() {
+                alert(message);
+            }
 
 // WHEN I answer a question THEN I am presented with another question
 
